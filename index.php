be5231d5fc5b8437215df795fc406e80c629b008
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-04-12 08:30:10 -0300

    Tela de fundo feita, arrumando css
    .

--- index.php
+++ index.php
@@ -13,9 +13,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
-    <header>
-     <nav class="navbar navbar-expand-lg navbar-light bg-red">
-    <a class="navbar-brand" href="#">Navbar</a>
+  <div id="cor"></div>
+    <header >
+     <nav class="navbar navbar-expand-lg navbar-light bg-red" style="background-image: linear-gradient(rgba(0, 0, 0, 0.200), rgba(0, 0, 0, 0.150), rgba(0, 0, 0, 0))";>
+    <a class="navbar-brand" href="#">FGR  </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -39,7 +40,7 @@
 
     </header>
     <main>
-    <div class="jumbotron">
+    <div class="jumbotron" class="d-none"  >
       <h1 class="display-4">Resumo do Teste</h1>
       <p class="lead">O que são testes de API? Os desenvolvedores fazem testes para saber se as APIs atendem às expectativas quanto a funcionalidade, desempenho, confiabilidade e segurança. A intenção é encontrar bugs e outros comportamentos inesperados para que os usuários não recebam um produto ruim ou perigoso.</p>
       <hr class="my-4">
