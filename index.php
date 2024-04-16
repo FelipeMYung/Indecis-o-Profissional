9a75216d950d798ad36c51c16fc7ddc06d196359
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-04-16 10:12:12 -0300

    pagina principal "estilo"
    .

--- index.php
+++ index.php
@@ -6,45 +6,39 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+     
 </head>
+  
+   
 <body>
-    <header>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-</head>
-<body>
-  <div id="cor"></div>
+  <script src="script.js"></script>
     <header >
-     <nav class="navbar navbar-expand-lg navbar-light bg-red" style="background-image: linear-gradient(rgba(0, 0, 0, 0.200), rgba(0, 0, 0, 0.150), rgba(0, 0, 0, 0))";>
-    <a class="navbar-brand" href="#">FGR  </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button id="drawer">
+      <div class="iconmenu"></div>
+      <div class="iconmenu"></div>
+      <div class="iconmenu"></div>
     </button>
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Arquivo</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Teste de api</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link">Fundadores</a>
-      </li>
-     
-     
-    </ul>
-</nav>
-
+    <div class="menu">
+        <button id="drawer2">
+          VOLTAR
+        </button>
+        <ul>
+        <a href="index.php"><li>Pagina inicial</li></a>
+        <a href=""><li>Informações</li></a>
+        <a href=""><li>Conteúdos</li></a>
+        <a href=""><li>Psicologia no cotidiano</li></a>
+        <a href=""><li>Dúvidas</li></a>
+        <a href=""><li>Sobre nós</li></a>
+      </ul>
+    </div>
+ 
     </header>
     <main>
-    <div class="jumbotron" class="d-none"  >
+    <div class="mid" class="bg-transparen"  >
       <h1 class="display-4">Resumo do Teste</h1>
       <p class="lead">O que são testes de API? Os desenvolvedores fazem testes para saber se as APIs atendem às expectativas quanto a funcionalidade, desempenho, confiabilidade e segurança. A intenção é encontrar bugs e outros comportamentos inesperados para que os usuários não recebam um produto ruim ou perigoso.</p>
       <hr class="my-4">
-      <button type="button" class="btn btn-secondary btn-lg btn-block">Vizualizar Artigo do Projeto</button>
+      <button type="button" class="btn btn-secondary btn-lg btn-block" >Vizualizar Artigo do Projeto</button>
     </div>
     </main>
     <footer> 
