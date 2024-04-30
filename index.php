0e9ad817951b0ac394d8228bf50c473936274282
Felipe Yung <102309048+FelipeMYung@users.noreply.github.com>
2024-04-30 11:08:31 -0300

    nao sei

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
       <div class="iconmenu"></div>
       <div class="iconmenu"></div>
     </button>
+    <h1>Indecisão Profissional na adolecência</h1>
     <div class="menu">
         <button id="drawer2">
           VOLTAR
@@ -31,13 +32,13 @@
     </header>
     <main>
       <div class="mid" class="bg-transparent"  >
-        <h1 class="display-1">Resumo do Projeto</h1>
+        <h2 class="display-1">Resumo do Projeto</h2>
         <p class="lead" class="text">nosso projeto é mt bom, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi dolorum possimus amet debitis a consequuntur deserunt explicabo, cumque voluptates repudiandae. Repellat, adipisci? Perspiciatis nihil ipsa hic quia itaque! Aliquam, blanditiis.</p>
         <hr class="my-3">
         <button type="button" class="btn-lg btn-block" class="bota">Vizualizar Artigo do Projeto</button>
       </div>
       <div class="mid" class="bg-transparent"  >
-        <h1 class="display-1">Resumo do Teste</h1>
+        <h2 class="display-1">Resumo do Teste</h2>
         <p class="lead" class="text">O que são testes de API? Os desenvolvedores fazem testes para saber se as APIs atendem às expectativas quanto a funcionalidade, desempenho, confiabilidade e segurança. A intenção é encontrar bugs e outros comportamentos inesperados para que os usuários não recebam um produto ruim ou perigoso.</p>
         <hr class="my-3">
         <button type="button" class="btn-lg btn-block" class="bota" >Vizualizar Teste</button>
